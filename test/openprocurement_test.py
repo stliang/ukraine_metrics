336f64ef5ee8dde5f5ab68ef20d34385941417fe
stliang <stliang@gmail.com>
2023-12-21 10:21:58 -0800

    fix sample tenders output data

--- test/openprocurement_test.py
+++ test/openprocurement_test.py
@@ -1,4 +1,6 @@
-import datetime
+#!/usr/bin/env python
+
+from datetime import datetime
 import json
 import pandas as pd
 import requests
@@ -8,12 +10,29 @@ date2 = '2018-02-01'
 
 mydates = pd.date_range(date1, date2).tolist()
 
-for s_date in mydates:
+uri="https://public.api.openprocurement.org/api/0/tenders"
+# uri="https://public.api.openprocurement.org/api/2.5/tenders?offset=1434981607.443577"
+# uri=f"https://public.api.openprocurement.org/api/0/tenders?offset={datetime.fromisoformat('2015-01-01T00:00:00+02:00').timestamp()}&descending=1&limit=10"
+response = requests.get(uri)
+#print(response.content)
+json_object = json.loads(response.content)
+json_formatted_str = json.dumps(json_object, indent=2)
+print(json_formatted_str)
+
+# for s_date in mydates:
+#     print(s_date.strftime("%y-%m-%d"))
+    
 	#uri="https://lb.api.openprocurement.org/api/0/tenders?offset=" + s_date.strftime("%y-%m-%d")
 	#uri="https://public.api.openprocurement.org/api/0/tenders?offset=" + s_date.strftime("%y-%m-%d")
-  uri="https://public.api.openprocurement.org/api/0/tenders
-	print(uri)
-	response = requests.get(uri)
-	result = json.loads(response.content)
-	for row in result["data"]:
-		print(row)
+	#uri="https://public.api.openprocurement.org/api/0/tenders"
+	#uri="https://public.api.openprocurement.org/api/0/tenders?offset=2015-01-01T00:00:00+02:00&descending=1&limit=10"
+	#uri="https://public.api.openprocurement.org/api/2.5/tenders?offset=1420063200.0&limit=1&descending=1"
+	#uri=f"https://public.api.openprocurement.org/api/0/tenders?offset={datetime.fromisoformat('2015-01-01T00:00:00+02:00').timestamp()}&descending=1&limit=10"
+	# uri="https://public.api.openprocurement.org/api/2.5/tenders?offset=1434981607.443577"
+	# response = requests.get(uri)
+	# #print(response.content)
+	# json_object = json.loads(response.content)
+	# json_formatted_str = json.dumps(json_object, indent=2)
+	# print(json_formatted_str)
+	#for row in result["data"]:
+	#	print(row)
